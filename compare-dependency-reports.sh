5dce69cd3a419c3439dc3f80524a80bb5d210a44
rbuisson <romain@mekomsolutions.com>
2023-07-28 13:31:16 +0200

    INFRA-405: Self-rebuild upon dependency changes using Maven Dependency Tracker plugin (#27)

---
+++ compare-dependency-reports.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+set -e
+
+# Download the remote dependency report
+remoteRepoUrl=https://nexus.mekomsolutions.net/repository/maven-public
+
+echo "Parsing details from project pom.xml..."
+version=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+artifactId=$(mvn help:evaluate -Dexpression=project.artifactId -q -DforceStdout)
+groupId=$(mvn help:evaluate -Dexpression=project.groupId -q -DforceStdout)
+classifier=$(mvn help:evaluate -Dexpression=dependencyReportClassifier -q -DforceStdout)
+artifact=${groupId}:${artifactId}:${version}::${classifier}
+file=/tmp/${artifactId}-${version}-${classifier}.txt
+
+echo "Fetch remote dependency report..."
+set +e
+mvn org.apache.maven.plugins:maven-dependency-plugin:3.2.0:get -DremoteRepositories=${remoteRepoUrl} -Dartifact=${artifact} -Dtransitive=false
+mvn org.apache.maven.plugins:maven-dependency-plugin:3.2.0:unpack -Dproject.basedir=/tmp/ -Dartifact=${artifact} -DoutputDirectory=/tmp/
+set -e
+
+# If no dependency report was fetched, create an empty one.
+if [ ! -f "$file" ]; then
+    touch $file
+fi
+
+# Build the local dependency report
+echo "Compile a local dependency report..."
+mvn clean compile
+
+# Compare the 2 files. Will exit with 0 if no change, 1 if changes
+echo "Compare both dependency reports..."
+set +e
+diff /tmp/ozone-distro-1.0.0-SNAPSHOT-dependencies.txt ./target/ozone-distro-1.0.0-SNAPSHOT-dependencies.txt
+diff_rc=$?
+set -e
+
+if [ $diff_rc -eq 0 ]
+then
+echo "No dependency change. Exit (0)"
+elif [ $diff_rc -eq 1 ]
+then
+echo "One or more dependency has changed. Exit (1)"
+else
+echo "Unknown error occured."
+fi
+
+echo hasChanged=$diff_rc >> "$GITHUB_OUTPUT"
+
+exit 0
