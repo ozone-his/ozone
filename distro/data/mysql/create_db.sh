195c217293fb35ec1846d7b7298ff5c742c32145
rbuisson <romain@mekomsolutions.com>
2023-10-24 12:50:23 +0200

    OZ-196: Split database creation scripts by service and provide util script

---
+++ distro/data/mysql/create_db.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Apply all scripts found in docker-entrypoint-initdb.d/db/ directory.
+#
+# Similar workaround as for the PostgreSQL Docker image not running files from sub-dirs.
+# https://github.com/docker-library/postgres/issues/605#issuecomment-567236795
+#
+#
+set -e
+directory=/docker-entrypoint-initdb.d/db/
+if [ -d "${directory}" ]
+then
+    find ${directory}* -type f -execdir ls {} \; -execdir bash {} \;
+else
+    echo "Directory '${directory}' does not exist. No database to create. Exit 0."
+fi
+exit 0
