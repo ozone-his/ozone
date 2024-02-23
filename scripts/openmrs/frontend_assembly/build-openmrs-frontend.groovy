abd7fcfcae7786995f199ba6c162af6b1f1753a0
Ian <ian.c.bacher@gmail.com>
2024-02-23 13:21:49 -0500

    OZ-462: Groovy script needs to support different output directories

--- scripts/openmrs/frontend_assembly/build-openmrs-frontend.groovy
+++ scripts/openmrs/frontend_assembly/build-openmrs-frontend.groovy
@@ -6,8 +6,17 @@ def refAppConfigFile = Paths.get("${project.build.directory}", "openmrs_frontend
 def slurper = new groovy.json.JsonSlurper()
 def refAppConfig = slurper.parse(refAppConfigFile)
 def openmrsVersion = refAppConfig["coreVersion"] ?: "next"
+def outputDirectory = "${project.groupId}" == "com.ozonehis" && "${project.artifactId}" == "ozone-distro" ?
+    "${project.build.directory}/${project.artifactId}-${project.version}/binaries/openmrs/frontend" :
+    "${project.build.directory}/${project.artifactId}-${project.version}/distro/binaries/openmrs/frontend"
 
-def assembleCommand = "npx --legacy-peer-deps openmrs@${openmrsVersion} assemble --manifest --mode config --target ${project.build.directory}/${project.artifactId}-${project.version}/binaries/openmrs/frontend --config ${refAppConfigFile.getAbsolutePath()}"
+def outputDirectoryFile = new File(outputDirectory)
+if (outputDirectoryFile.exists()) {
+    outputDirectoryFile.eachFile(it -> it.delete())
+    outputDirectoryFile.eachDir(it -> { if (it.getName() != "ozone") { it.deleteDir() } })
+}
+
+def assembleCommand = "npx --legacy-peer-deps openmrs@${openmrsVersion} assemble --manifest --mode config --target ${outputDirectory} --config ${refAppConfigFile.getAbsolutePath()}"
 log.info("Project: ${project.groupId}:${project.artifactId}")
 // by default, we build the frontend as part of Ozone and only rebuild if there are local customizations
 def shouldBuildFrontend = "${project.groupId}" == "com.ozonehis" && "${project.artifactId}" == "ozone-distro"
@@ -35,7 +44,7 @@ if (shouldBuildFrontend) {
 
     log.info("Running build command...")
 
-    def buildProcess = "npx --legacy-peer-deps openmrs@${openmrsVersion} build --config-url \$SPA_CONFIG_URLS --default-locale \$SPA_DEFAULT_LOCALE --target ${project.build.directory}/${project.artifactId}-${project.version}/binaries/openmrs/frontend".execute()
+    def buildProcess = "npx --legacy-peer-deps openmrs@${openmrsVersion} build --config-url \$SPA_CONFIG_URLS --default-locale \$SPA_DEFAULT_LOCALE --target ${outputDirectory}".execute()
     buildProcess.consumeProcessOutput(System.out, System.err)
     buildProcess.waitFor()
 
