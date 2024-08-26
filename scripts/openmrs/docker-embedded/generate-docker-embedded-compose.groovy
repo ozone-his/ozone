3416f76b39e0cebc505b2906150ae92fbe8322cd
Emmanuel Nyachoke <achachiez@gmail.com>
2024-08-26 19:02:08 +0300

    Update Image names

--- scripts/openmrs/docker-embedded/generate-docker-embedded-compose.groovy
+++ scripts/openmrs/docker-embedded/generate-docker-embedded-compose.groovy
@@ -4,14 +4,14 @@ import java.text.SimpleDateFormat
 import groovy.text.* 
 import groovy.json.JsonBuilder
 
-myver  = "${project.version}"
+myver  = "${project.groupId}-${project.artifactId}-${project.version}"
 
-def dockerComposeTemplate = Paths.get("${project.build.directory}/${project.artifactId}-${project.version}", "docker-embedded", "docker-compose-embedded.yaml.template").toFile()
+def dockerComposeTemplate = Paths.get("${project.build.directory}", "/embedded-build-tmp", "docker-embedded", "docker-compose-embedded.yaml.template").toFile()
 def binding = ['dockertag' : myver ]			  
 def engine = new SimpleTemplateEngine() 
 def template = engine.createTemplate(dockerComposeTemplate) 
 def writable = template.make(binding) 
 
-def dockerComposePath = Paths.get("${project.build.directory}/${project.artifactId}-${project.version}", "docker-embedded", "docker-compose-embedded.yaml").toAbsolutePath().toString()
+def dockerComposePath = Paths.get("${project.build.directory}", "/embedded-build-tmp", "docker-embedded", "docker-compose-embedded.yaml").toAbsolutePath().toString()
 def myFile = new File(dockerComposePath)
 myFile.write(writable.toString())
