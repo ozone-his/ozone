da31c1624c6ae96c41b62e28a2de0e4b3c37d436
Emmanuel Nyachoke <achachiez@gmail.com>
2024-08-28 17:54:58 +0300

    Refactor

--- scripts/openmrs/bundled-docker/generate-bundled-docker-compose.groovy
+++ scripts/openmrs/bundled-docker/generate-bundled-docker-compose.groovy
@@ -6,12 +6,12 @@ import groovy.json.JsonBuilder
 
 myver  = "${project.groupId}-${project.artifactId}-${project.version}"
 
-def dockerComposeTemplate = Paths.get("${project.build.directory}", "/embedded-build-tmp", "docker-embedded", "docker-compose-embedded.yaml.template").toFile()
+def dockerComposeTemplate = Paths.get("${project.build.directory}", "/bundled-docker-build-tmp", "bundled-docker", "docker-compose-bundled.yaml.template").toFile()
 def binding = ['dockertag' : myver ]			  
 def engine = new SimpleTemplateEngine() 
 def template = engine.createTemplate(dockerComposeTemplate) 
 def writable = template.make(binding) 
 
-def dockerComposePath = Paths.get("${project.build.directory}", "/embedded-build-tmp", "docker-embedded", "docker-compose-embedded.yaml").toAbsolutePath().toString()
+def dockerComposePath = Paths.get("${project.build.directory}", "/bundled-docker-build-tmp", "bundled-docker", "docker-compose-bundled.yaml").toAbsolutePath().toString()
 def myFile = new File(dockerComposePath)
 myFile.write(writable.toString())
