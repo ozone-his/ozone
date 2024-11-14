0b598ced889a5c4cd23382d81f5baff0c21c2039
ruhanga <ruhanganathan@gmail.com>
2024-11-14 13:43:27 +0300

    OZ-741: Added Bahmni EMR to Ozone

---
+++ distro/configs/bahmni/bahmniemr_config/apps/registration/fieldValidation.js
@@ -0,0 +1,8 @@
+Bahmni.Registration.customValidator = {
+    "age.days": {
+        method: function (name, value) {
+            return value >= 0;
+        },
+        errorMessage: "REGISTRATION_AGE_ERROR_KEY"
+    }
+};
