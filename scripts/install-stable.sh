4b5ebe823eb8d12755bb3ead8e97853c629b1a9f
rbuisson <romain@mekomsolutions.com>
2025-06-10 16:48:50 +0200

    Update quick install script to download Maven 3.9.10

--- scripts/install-stable.sh
+++ scripts/install-stable.sh
@@ -44,7 +44,7 @@ fi
 
 # Download Maven and install locally
 echo "$INFO Installing Maven $mavenVersion..."
-mavenVersion="3.9.6"
+mavenVersion="3.9.10"
 mvn=apache-maven-${mavenVersion}/bin/mvn
 if [ -f "$mvn" ]
 then
