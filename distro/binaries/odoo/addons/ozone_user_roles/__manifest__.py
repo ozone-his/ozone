51c2e33a73fd9fb390952273529ccb56b8f0a141
wluyima <wyclif@openmrs.org>
2025-11-28 10:03:03 -0600

    MG-24: Apply roles set in Keycloak to Odoo

---
+++ distro/binaries/odoo/addons/ozone_user_roles/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Ozone User Roles",
+    "version": "17.0.0.0.0",
+    "license": "AGPL-3",
+    "author": (
+        "Mekom Solutions"
+    ),
+    "website": "https://github.com/ozone-his/ozone-auth-roles/odoo",
+    "summary": "Assigns a user roles from the authentication provider upon login",
+    "depends": ["auth_oidc"],
+    "data": [],
+    "demo": [],
+    "installable": True,
+    "auto_install": True
+}
