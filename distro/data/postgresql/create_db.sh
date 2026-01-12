676537ca0ada7f507fa4c210b1aed7e476b8ad73
Kipchumba Bett <kipchubett@gmail.com>
2026-01-12 11:24:49 +0300

    MG-4: Add OpenELIS db setup script and update PostgreSQL init process (#226)

--- distro/data/postgresql/create_db.sh
+++ distro/data/postgresql/create_db.sh
@@ -5,12 +5,18 @@
 # Workaround for PostgreSQL Docker image not running files from sub-dirs.
 # https://github.com/docker-library/postgres/issues/605#issuecomment-567236795
 #
+# Only execute shell scripts (*.sh) found in the subdirectories so that
+# SQL files are not interpreted by the shell (they should be applied via
+# psql from the shell scripts that know how to apply them).
 #
 set -e
 directory=/docker-entrypoint-initdb.d/db/
 if [ -d "${directory}" ]
 then
-    find ${directory}* -type f -execdir ls {} \; -execdir bash {} \;
+    # Find and execute only shell scripts. execdir runs the command from the
+    # directory where the matched file is located, which preserves relative
+    # paths used by the scripts.
+    find "${directory}" -type f -name '*.sh' -execdir ls {} \; -execdir bash {} \;
 else
     echo "Directory '${directory}' does not exist. No database to create. Exit 0."
 fi
